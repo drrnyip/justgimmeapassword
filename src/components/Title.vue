e6d92ffc7331fa54056774898ba1ab63a4d94086
Darren Yip <drrnyip@gmail.com>
2022-03-24 21:32:18 -0400

    feat: refactor title to be more efficient

--- src/components/Title.vue
+++ src/components/Title.vue
@@ -1,201 +1,22 @@
 <template>
   <h1 class="title flex">
-    <div class="row">
-      <p
-        @mouseover="hover('char1')"
-        @mouseout="stopHover('char1')"
-        :class="{ animate: isHovering('char1'), fallout: falloutNow('char1') }"
-        class="letter"
-      >
-        j
-      </p>
-      <p
-        @mouseover="hover('char2')"
-        @mouseout="stopHover('char2')"
-        :class="{ animate: isHovering('char2'), fallout: falloutNow('char2') }"
-        class="letter"
-      >
-        u
-      </p>
-      <p
-        @mouseover="hover('char3')"
-        @mouseout="stopHover('char3')"
-        :class="{ animate: isHovering('char3'), fallout: falloutNow('char3') }"
-        class="letter"
-      >
-        s
-      </p>
-      <p
-        @mouseover="hover('char4')"
-        @mouseout="stopHover('char4')"
-        :class="{ animate: isHovering('char4'), fallout: falloutNow('char4') }"
-        class="letter"
-      >
-        t
-      </p>
-      <p
-        @mouseover="hover('char5')"
-        @mouseout="stopHover('char5')"
-        :class="{ animate: isHovering('char5') }"
-        class="letter blank"
-      >
-        _
-      </p>
-      <p
-        @mouseover="hover('char6')"
-        @mouseout="stopHover('char6')"
-        :class="{ animate: isHovering('char6') }"
-        class="letter"
-      >
-        g
-      </p>
-      <p
-        @mouseover="hover('char7')"
-        @mouseout="stopHover('char7')"
-        :class="{ animate: isHovering('char7') }"
-        class="letter"
-      >
-        i
-      </p>
-      <p
-        @mouseover="hover('char8')"
-        @mouseout="stopHover('char8')"
-        :class="{ animate: isHovering('char8') }"
-        class="letter"
-      >
-        m
-      </p>
-      <p
-        @mouseover="hover('char9')"
-        @mouseout="stopHover('char9')"
-        :class="{ animate: isHovering('char9') }"
-        class="letter"
-      >
-        m
-      </p>
-      <p
-        @mouseover="hover('char10')"
-        @mouseout="stopHover('char10')"
-        :class="{
-          animate: isHovering('char10'),
-        }"
-        class="letter"
-      >
-        e
-      </p>
-      <p
-        @mouseover="hover('char11')"
-        @mouseout="stopHover('char11')"
-        :class="{
-          animate: isHovering('char11'),
-        }"
-        class="letter blank"
-      >
-        _
-      </p>
-      <p
-        @mouseover="hover('char12')"
-        @mouseout="stopHover('char12')"
-        :class="{
-          animate: isHovering('char12'),
-          fallout: falloutNow('char12'),
-        }"
-        class="letter"
-      >
-        a
-      </p>
-      <p
-        @mouseover="hover('char13')"
-        @mouseout="stopHover('char13')"
-        :class="{
-          animate: isHovering('char13'),
-        }"
-        class="letter blank"
-      >
-        _
-      </p>
-    </div>
-    <div class="row">
-      <p
-        @mouseover="hover('char14')"
-        @mouseout="stopHover('char14')"
-        :class="{
-          animate: isHovering('char14'),
-        }"
-        class="letter"
-      >
-        p
-      </p>
-      <p
-        @mouseover="hover('char15')"
-        @mouseout="stopHover('char15')"
-        :class="{
-          animate: isHovering('char15'),
-        }"
-        class="letter"
-      >
-        a
-      </p>
-      <p
-        @mouseover="hover('char16')"
-        @mouseout="stopHover('char16')"
-        :class="{
-          animate: isHovering('char16'),
-        }"
-        class="letter"
-      >
-        s
-      </p>
-      <p
-        @mouseover="hover('char17')"
-        @mouseout="stopHover('char17')"
-        :class="{
-          animate: isHovering('char17'),
-        }"
-        class="letter"
-      >
-        s
-      </p>
-      <p
-        @mouseover="hover('char18')"
-        @mouseout="stopHover('char18')"
-        :class="{
-          animate: isHovering('char18'),
-        }"
-        class="letter"
-      >
-        w
-      </p>
-      <p
-        @mouseover="hover('char19')"
-        @mouseout="stopHover('char19')"
-        :class="{
-          animate: isHovering('char19'),
-        }"
-        class="letter"
-      >
-        o
-      </p>
-      <p
-        @mouseover="hover('char20')"
-        @mouseout="stopHover('char20')"
-        :class="{
-          animate: isHovering('char20'),
-        }"
-        class="letter"
-      >
-        r
-      </p>
-      <p
-        @mouseover="hover('char21')"
-        @mouseout="stopHover('char21')"
-        :class="{
-          animate: isHovering('char21'),
-        }"
-        class="letter"
-      >
-        d
-      </p>
+    <div v-for="(words, index) in rows" :key="index" class="row">
+      <template v-for="word in words" :key="word">
+        <p
+          v-for="letter in word.entry.split('')"
+          :key="letter"
+          @mouseover="hover(`${index}${word}${letter}`)"
+          @mouseout="stopHover(`${index}${word}${letter}`)"
+          :class="{
+            animate: isHovering(`${index}${word}${letter}`),
+            fallout: word.fallout && falloutNow(`${index}${word}${letter}`),
+          }"
+          class="letter"
+        >
+          {{ letter }}
+        </p>
+      <p class="letter blank">_</p>
+      </template>
     </div>
   </h1>
 </template>
@@ -208,6 +29,15 @@ export default {
       hovering: {},
       fallout: {},
       maxHover: 20,
+      rows: [
+        // Array of arrays. Each inner array is a flex row
+        [
+          { entry: "just", fallout: true },
+          { entry: "gimme", fallout: false },
+          { entry: "a", fallout: true },
+        ],
+        [{ entry: "password", fallout: false }],
+      ],
     };
   },
   methods: {
